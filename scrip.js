68a96bf62cbbcff7968a119d334534d7944983fc
Maxim <toper.one111@gmail.com>
2022-09-20 18:18:08 +0300

    Create loading page

---
+++ scrip.js
@@ -0,0 +1,6 @@
+const startGamePlVsPlBtn = document.querySelector('.pvsp');
+
+startGamePlVsPlBtn.addEventListener('click', () => {
+    const loadingWindow = document.querySelector('.loading-window');
+    loadingWindow.style.display = 'none';
+})
